7737a6d7c33b1db5f0e7156743a569270d3e717e
Linda Trost <ltrost0910@aol.com>
2024-09-08 14:17:44 -0600

    change hero image to webp

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,9 +2,9 @@
 import "bootstrap"
 
 //preload hero image
-function preload_image(im_url) {
-  let img = new Image()
-  img.src = im_url
-}
+// function preload_image(im_url) {
+//   let img = new Image()
+//   img.src = im_url
+// }
 
-preload_image("/images/ford_fusion.png")
+// preload_image("/images/ford_fusion.png")
