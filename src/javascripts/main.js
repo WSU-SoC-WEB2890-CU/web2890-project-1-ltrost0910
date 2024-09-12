7515e99b68f9a2406c8052b0d105caab20d5ad8f
Linda Trost <ltrost0910@aol.com>
2024-09-12 14:12:36 -0600

    trying to keep testimonial carousel from changing height based on number of lines in text

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,2 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
-import "bootstrap"
-
-//preload hero image
-// function preload_image(im_url) {
-//   let img = new Image()
-//   img.src = im_url
-// }
-
-// preload_image("/images/ford_fusion.webp")
+import "bootstrap" // Import Bootstrap JavaScript
